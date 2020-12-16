8ed941f5acaae28e2495b749a0ca1c8096632c24
Joe Chea <cheasingh168@gmail.com>
2020-12-17 01:07:11 +0700

    flash-card update save and correct word excluded

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 from tkinter import Tk, Button, Entry, Label, Canvas, PhotoImage
 import pandas
 from random import choice
-import json
 
 BACKGROUND_COLOR = "#B1DDC6"
+timer = None
+card_status = 0
+
 
 data = pandas.read_csv("./data/french_words.csv")
-# test = data[data.French == "mois"].English
-# print(str(test.values[0]))
-
-# print(choice(data.French))
 
 # -----------------------------Function----------------------------
 
 
+def save_answer(ans):
+
+    with open("./data/known_word.csv", "a") as df:
+        df.write(f"{ans},{data[data.French == ans].English.values[0]}\n")
+
+
+def known_answer():
+    global card_status
+
+    if card_status == 0:
+        word = canvas.itemcget(answer_text, "text")
+        save_answer(word)
+        gen_word()
+        window.after_cancel(timer)
+    else:
+        pass
+
+
+def unknown_answer():
+    gen_word()
+    flip_card(False)
+
+
 def gen_word():
-    canvas.itemconfig(answer_text, text=f"{choice(data.French)}")
+    try:
+
+        with open("./data/known_word.csv", "r") as file:
+            learn_words = [i.split(",")[0] for i in file.read().split("\n")]
+
+    except FileNotFoundError:
+        learn_words = []
+
+    new_data = [ans for ans in data.French if ans not in learn_words]
+
+    word = choice(new_data)
+    canvas.itemconfig(answer_text, text=f"{word}")
+    return word
 
 
 def translate(language, text):
@@ -30,6 +63,8 @@ def translate(language, text):
 
 
 def flip_card(front):
+    global timer
+    global card_status
 
     if front == True:
         canvas.itemconfig(language_text, text="English")
@@ -37,16 +72,18 @@ def flip_card(front):
 
         french = canvas.itemcget(answer_text, "text")
         canvas.itemconfig(answer_text, text=translate("English", french))
+        card_status = 1
 
     else:
         canvas.itemconfig(card, image=img_front)
         canvas.itemconfig(language_text, text="French")
         gen_word()
+        card_status = 0
 
-    window.after(3000, flip_card, not front)
+    timer = window.after(3000, flip_card, not front)
 
 
-    # -----------------------------UI Design---------------------------
+# -----------------------------UI Design---------------------------
 window = Tk()
 window.title("App Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -79,13 +116,14 @@ canvas.grid(row=0, column=0, columnspan=2)
 # right button
 img_right = PhotoImage(file="./images/right.png")
 btn_right = Button(image=img_right, width=80, height=80,
-                   highlightthickness=0, command=gen_word)
+                   highlightthickness=0, command=known_answer)
 btn_right.grid(row=1, column=0)
+
 
 # wrong button
 img_wrong = PhotoImage(file="./images/wrong.png")
 btn_wrong = Button(image=img_wrong, width=80, height=80,
-                   highlightthickness=0, command=gen_word)
+                   highlightthickness=0, command=unknown_answer)
 btn_wrong.grid(row=1, column=1)
 
 
